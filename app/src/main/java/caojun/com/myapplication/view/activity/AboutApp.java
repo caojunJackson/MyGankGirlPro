dad5041f77bd69d1022b220f85125eb8f66dea39
asmouse <asmouse00320@gmail.com>
2017-03-07 15:43:46 +0800

    v1.0.0完成

--- app/src/main/java/caojun/com/myapplication/view/activity/AboutApp.java
+++ app/src/main/java/caojun/com/myapplication/view/activity/AboutApp.java
@@ -2,9 +2,26 @@ package caojun.com.myapplication.view.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import caojun.com.myapplication.R;
+import caojun.com.myapplication.model.GanHuo;
+import caojun.com.myapplication.retrofit.GankRetrofit;
+import caojun.com.myapplication.retrofit.GankServer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by tiger on 2017/3/6.
@@ -12,11 +29,86 @@ import caojun.com.myapplication.R;
 
 public class AboutApp extends AppCompatActivity {
 
+    @BindView(R.id.image_about)
+    ImageView mImageAbout;
+    @BindView(R.id.aboutapp_toolbar)
+    Toolbar mAboutappToolbar;
+    @BindView(R.id.aboutapp_appbar)
+    AppBarLayout mAboutappAppbar;
+    @BindView(R.id.tv_appdesc)
+    TextView mTvAppdesc;
+    @BindView(R.id.aboutapp_fab)
+    FloatingActionButton mAboutappFab;
+    @BindView(R.id.aboutapp_collapsing)
+    CollapsingToolbarLayout mAboutappCollapsing;
+
+    private int page = 1;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_aboutapp);
+        ButterKnife.bind(this);
+
+        init();
+
+    }
+
+    private void init() {
+        setImageBar();
+
+        mAboutappCollapsing.setTitle("About App");
+        setSupportActionBar(mAboutappToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mAboutappAppbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+
+                int maxScroll = appBarLayout.getTotalScrollRange();
+                float percentage = (float) Math.abs(verticalOffset) / (float) maxScroll;
+
+                if(percentage > 0.9f){
+                    page++;
+                    setImageBar();
+                }
+            }
+        });
+
+        mTvAppdesc.setText("MyGankGrilPro");
+    }
+
+    private void setImageBar() {
+        GankRetrofit.getRetrofit()
+                .create(GankServer.class)
+                .getGanHuo("福利", 1, page)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<GanHuo>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Glide.with(getApplicationContext())
+                                .load(R.drawable.wall_picture)
+                                .into(mImageAbout);
+                    }
+
+                    @Override
+                    public void onNext(GanHuo ganHuo) {
+
+                        Glide.with(getApplicationContext())
+                                .load(ganHuo.getResults().get(0).getUrl())
+                                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                                .centerCrop()
+                                .into(mImageAbout);
+                    }
+                });
+
     }
 
 }
--- app/src/main/java/caojun/com/myapplication/view/activity/AboutMe.java
+++ app/src/main/java/caojun/com/myapplication/view/activity/AboutMe.java
@@ -1,12 +1,65 @@
 package caojun.com.myapplication.view.activity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import caojun.com.myapplication.R;
+
 
 /**
- * Created by tiger on 2017/3/6.
+ * Created by Admin on 2016/5/22.
  */
-
 public class AboutMe extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_me);
+
+        //Toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("关于我");
+
+        findViewById(R.id.go_github).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/caojunJackson/MyGankGirlPro")));
+            }
+        });
 
 
+        findViewById(R.id.go_gankgirl).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://blog.csdn.net/davagent")));
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_about_me, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_click_me) {
+            Snackbar.make(toolbar, "这里什么都没有.. ( ＞ω＜)", Snackbar.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
